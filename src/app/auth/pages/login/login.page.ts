22cf0d677d3169720de24c7c00a270be7de71031
victor27andre <victor27andre@gmail.com>
2019-12-26 12:12:17 -0300

    aula 8. formatador de templates angular

--- src/app/auth/pages/login/login.page.ts
+++ src/app/auth/pages/login/login.page.ts
@@ -1,15 +1,39 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.page.html',
-  styleUrls: ['./login.page.scss'],
+    selector: 'app-login',
+    templateUrl: './login.page.html',
+    styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+    authForm: FormGroup;
 
-  ngOnInit() {
-  }
+    constructor(private fb: FormBuilder) { }
+
+    ngOnInit(): void {
+        this.createForm();
+    }
+
+    private createForm(): void {
+        this.authForm = this.fb.group({
+            email: ['', [Validators.required, Validators.email]],
+            password: ['', [Validators.required, Validators.minLength(6)]]
+        });
+    }
+
+    get email(): FormControl {
+        return this.authForm.get('email') as FormControl;
+    }
+    get password(): FormControl {
+        return this.authForm.get('password') as FormControl;
+    }
+
+    onSubmit(): void {
+        console.log('AuthForm: ', this.authForm.value);
+    }
+
+
 
 }
