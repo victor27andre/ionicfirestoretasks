58aa441858bcbb49aee5a56e5f773858189ae57d
victor27andre <victor27andre@gmail.com>
2019-12-28 11:32:07 -0300

    aula 29. criando aauthservice com ionic cli

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyAV6vllGv1llQsDDt3A0cDdezdof9Kzxl4',
+    authDomain: 'ionic-firestore-tasks-77fb5.firebaseapp.com',
+    databaseURL: 'https://ionic-firestore-tasks-77fb5.firebaseio.com',
+    projectId: 'ionic-firestore-tasks-77fb5',
+    storageBucket: 'ionic-firestore-tasks-77fb5.appspot.com',
+    messagingSenderId: '1056558635856',
+    appId: '1:1056558635856:web:f930096abe1405a11d2a55',
+    measurementId: 'G-BGF9ZHVFWB'
+  }
 };
 
 /*
