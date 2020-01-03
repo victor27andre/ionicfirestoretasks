b34e4840aa9ce3623b615a633e6477f5ec66f141
victor27andre <victor27andre@gmail.com>
2020-01-03 09:47:08 -0300

    aula 31. AuthService: implementando signup com Email e Password

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { auth } from 'firebase/app';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(private afAuth: AngularFireAuth) { }
+
+  private signInWithEmail({ email, password }): Promise<auth.UserCredential> {
+    return this.afAuth.auth.signInWithEmailAndPassword(email, password);
+  }
+
+  private signUpWithEmail({ email, password, name }): Promise<auth.UserCredential> {
+    return this.afAuth.auth
+      .createUserWithEmailAndPassword(email, password)
+      .then(credentials =>
+        credentials.user
+          .updateProfile({ displayName: name, photoURL: null })
+          .then(() => credentials)
+      );
+  }
+
 }
+
